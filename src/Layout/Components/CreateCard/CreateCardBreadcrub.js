47479d2d347872edc21a524de8111eb8b2d88807
jonathan6ramirez <84485168+jonathan6ramirez@users.noreply.github.com>
2021-12-08 15:37:25 -0600

    Added the templete breadcrumb

--- src/Layout/Components/CreateCard/CreateCardBreadcrub.js
+++ src/Layout/Components/CreateCard/CreateCardBreadcrub.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function BreadCrumb () {
-
+    return (
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item text-primary">Library</li>
+                <li class="breadcrumb-item active" aria-current="page">Data</li>
+            </ol>
+        </nav>
+    )
 }
 
 export default BreadCrumb;
